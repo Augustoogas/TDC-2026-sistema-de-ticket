e8a902a5e80103b234c650d6a20303778425b956
DanOlmedo <oxdeo85@gmail.com>
2026-04-30 19:53:59 -0300

    feat: implementacion ticketcontroller y correccion de mapeos de entidades

--- backend/src/main/java/com/unpaz/backend/model/Locacion.java
+++ backend/src/main/java/com/unpaz/backend/model/Locacion.java
@@ -1,9 +1,6 @@
 package com.unpaz.backend.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,10 +14,11 @@ public class Locacion {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idLocacion; 
+   
+    // SE ELIMINÓ: private Locacion locacion; (Esto causaba el error)
+    
     private String nombre;
     private String direccion;
     private int capacidad;
-    
     private String asientos; 
-
 }
