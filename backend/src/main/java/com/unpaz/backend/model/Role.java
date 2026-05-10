240ff452685026debf54a721f0370ce9a91bea93
DanOlmedo <oxdeo85@gmail.com>
2026-05-10 11:28:45 -0300

    feat: implementacion de spring security configuracion de dependencias y entidad usuario

---
+++ backend/src/main/java/com/unpaz/backend/model/Role.java
@@ -0,0 +1,6 @@
+package com.unpaz.backend.model;
+
+public enum Role {
+	ADMIN,
+	CLIENTE
+}
--- backend/src/main/java/com/unpaz/backend/model/Usuario.java
+++ backend/src/main/java/com/unpaz/backend/model/Usuario.java
@@ -1,27 +1,52 @@
 package com.unpaz.backend.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Inheritance;
-import jakarta.persistence.InheritanceType;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Entity
+@Inheritance(strategy = InheritanceType.JOINED)
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Inheritance(strategy = InheritanceType.JOINED) // Crea tablas separadas unidas por ID
-public abstract class Usuario {
+public class Usuario implements UserDetails { // <-- esta implementacion establece la utilizacion de spring security
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long usuarioId;
+
     private String nombre;
-    @Column(unique = true, nullable = false)
+    private String apellido;
+    
+    @Column(unique = true) // El email no se puede repetir
     private String email;
-    private String contrasenia;
+    
+    private String password;
+
+    // Aquí definimos qué rol tiene el usuario. 
+    @Enumerated(EnumType.STRING)
+    private Role role; 
+
+    // --- MÉTODOS DE USERDETAILS ---
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        // Esto le dice a Spring que permisos tiene (ej: ROLE_ADMIN o ROLE_CLIENTE)
+        return List.of(new SimpleGrantedAuthority("ROLE_" + role.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return email; // Usamos el email como "username" para el login
+    }
+
+    @Override public boolean isAccountNonExpired() { return true; }
+    @Override public boolean isAccountNonLocked() { return true; }
+    @Override public boolean isCredentialsNonExpired() { return true; }
+    @Override public boolean isEnabled() { return true; }
 }
