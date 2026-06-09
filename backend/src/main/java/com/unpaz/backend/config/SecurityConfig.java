bd6ff0cf24bbddb6eec05c3dcac3ce0de514bf9a
luciel <sotoluciamag@gmail.com>
2026-06-09 13:31:58 -0300

    fix: configurar cors en el securityconfig

--- backend/src/main/java/com/unpaz/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/unpaz/backend/config/SecurityConfig.java
@@ -37,7 +37,7 @@ public class SecurityConfig {
         return http
             .cors(cors -> cors.configurationSource(request -> {
                 var corsConfiguration = new org.springframework.web.cors.CorsConfiguration();
-                corsConfiguration.setAllowedOrigins(java.util.List.of("http://localhost:5173", "http://127.0.0.1:5173")); // Tu puerto de Vite
+                corsConfiguration.setAllowedOrigins(java.util.List.of("http://localhost:5173"));
                 corsConfiguration.setAllowedMethods(java.util.List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
                 corsConfiguration.setAllowedHeaders(java.util.List.of("*"));
                 corsConfiguration.setAllowCredentials(true);
